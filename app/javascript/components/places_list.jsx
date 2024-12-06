42b03a519e7df5965a6acb664de8cb9e63ed28e9
AMT98 <aswinmalla12@gmail.com>
2024-12-05 20:13:10 -0700

    Able to fetch api/places route and display data

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,38 +1,49 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
 function PlacesList() {
   const [loading, setLoading] = useState(true);
   const [loadedPlaces, setLoadedPlaces] = useState([]);
-  
-  const loadingSection = (<div>Loading...</div>);
-  const dataSection = loadedPlaces.map((place, index) =>         <div key={index}>
-  <table>
-    <tr>
-      <th>Name</th>
-      <th>City</th>
-      <th>Recent Upload Speed</th>
-      <th>Recent Upload Speed Units</th>
-      <th>Number of measurements</th>
-    </tr>
-    <tr>
-      <td>{place.name}</td>
-      <td>{place.city}</td>
-      <td>{place.most_recent_avg_download_speed}</td>
-      <td>{place.most_recent_download_speed_units}</td>
-      <td>{place.number_of_measurements}</td>
-    </tr>
-  </table>
-  </div> )
 
-  if(loading){
-    return loadingSection
-  }else {
-    return dataSection
+  useEffect(() => {
+    const apiEndpoint = "/api/places";
+    fetch(apiEndpoint)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setLoadedPlaces(data["places"]);
+        setLoading(false);
+      });
+  }, []);
+
+  const loadingSection = <div>Loading...</div>;
+  const dataSection = loadedPlaces.map((place, index) => (
+    <div key={index}>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>City</th>
+          <th>Recent Upload Speed</th>
+          <th>Recent Upload Speed Units</th>
+          <th>Number of measurements</th>
+        </tr>
+        <tr>
+          <td>{place.name}</td>
+          <td>{place.city}</td>
+          <td>{place.most_recent_avg_download_speed}</td>
+          <td>{place.most_recent_download_speed_units}</td>
+          <td>{place.number_of_measurements}</td>
+        </tr>
+      </table>
+    </div>
+  ));
+
+  if (loading) {
+    return loadingSection;
+  } else {
+    return dataSection;
   }
-    
-  }
-  
+}
 
 const placesList = ReactDOM.createRoot(
   document.getElementById("places-list-container")
