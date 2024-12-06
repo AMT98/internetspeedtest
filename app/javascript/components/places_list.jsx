9de5ee334ec8a39c09934fbbe855ce4ef162472e
AMT98 <aswinmalla12@gmail.com>
2024-12-05 19:41:45 -0700

    Removed node modules and reinstalled it

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom/client";
+
+function PlacesList() {
+  
+}
 
 class PlacesList extends React.Component {
   render() {
