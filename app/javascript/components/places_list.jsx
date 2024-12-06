684992d1d8eb7bc0b9212583cadbafb423100d1d
AMT98 <aswinmalla12@gmail.com>
2024-12-05 20:05:19 -0700

    Updated react version

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -2,14 +2,37 @@ import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom/client";
 
 function PlacesList() {
+  const [loading, setLoading] = useState(true);
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
   
-}
+  const loadingSection = (<div>Loading...</div>);
+  const dataSection = loadedPlaces.map((place, index) =>         <div key={index}>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>City</th>
+      <th>Recent Upload Speed</th>
+      <th>Recent Upload Speed Units</th>
+      <th>Number of measurements</th>
+    </tr>
+    <tr>
+      <td>{place.name}</td>
+      <td>{place.city}</td>
+      <td>{place.most_recent_avg_download_speed}</td>
+      <td>{place.most_recent_download_speed_units}</td>
+      <td>{place.number_of_measurements}</td>
+    </tr>
+  </table>
+  </div> )
 
-class PlacesList extends React.Component {
-  render() {
-    return <div>Places list rendered in react!</div>;
+  if(loading){
+    return loadingSection
+  }else {
+    return dataSection
   }
-}
+    
+  }
+  
 
 const placesList = ReactDOM.createRoot(
   document.getElementById("places-list-container")
